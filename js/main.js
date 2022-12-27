049ad27d416a84e251c2d8dd5eaea5b17dc5e0ae
nick-is-coding <96067193+nick-is-coding@users.noreply.github.com>
2022-12-27 13:33:28 -0600

    theme toggler
    - enable functionality to theme toggler that will set theme to either light or dark

--- js/main.js
+++ js/main.js
@@ -13,14 +13,49 @@ const isVisible = 'is-visible';
 
 const root = document.documentElement;
 
-/*  Theme  */ 
+// Theme
 const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn); // will take everything within the document with that label and create a node array list 
 const currentTheme = localStorage.getItem(theme);
 
-
+// Modal
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
+
+const setActive = (elm, selector) => {
+    if (document.querySelector(`${selector}.${active}`) !== null) {
+        document.querySelector(`${selector}.${active}`).classList.remove(active);
+    } else {
+        elm.classList.add(active);
+    }
+};
+
+const setTheme = (val) => {
+    if (val === dark) {
+        root.setAttribute(dataTheme, dark);
+        localStorage.setItem(theme, dark);
+    } else {
+        root.setAttribute(dataTheme, light);
+        localStorage.setItem(theme, light);
+    }
+};
+
+toggleTheme.addEventListener('click', function() {
+    const tab = this.parentElement.parentElement;
+    if (!tab.className.includes(open)) {
+        tab.classList.add(open);
+    } else {
+        tab.classList.remove(open);
+    }
+});
+
+for (const elm of switcher) {
+    elm.addEventListener('click', function() {
+        const toggle = this.dataset.toggle;
+        setActive(elm, switcherBtn);
+        setTheme(toggle);
+    })
+};
 
 // Full site modal "Open Buttons"
 for(const elm of openModal) {
@@ -28,11 +63,11 @@ for(const elm of openModal) {
         const modalId = this.dataset.open;
         document.getElementById(modalId).classList.add(isVisible);
     })
-}
+};
 // Full site modal "Close Buttons"
 for (const elm of closeModal) {
     elm.addEventListener('click', function() {
         this.parentElement.parentElement.classList.remove(isVisible);
     } )
-}
+};
 
